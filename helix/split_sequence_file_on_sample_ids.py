5a9baaef1a0a98dcb1d39c123eb30069adce4518
bhillmann <hillmannben@gmail.com>
2021-01-15 10:37:18 -0600

    updated the split sequence file on sample ids

--- helix/split_sequence_file_on_sample_ids.py
+++ helix/split_sequence_file_on_sample_ids.py
@@ -11,8 +11,8 @@ def make_arg_parser():
         description='This is the commandline interface for split_sequence_file_on_sample_ids',
     )
     parser.add_argument('-f', '--fasta', help='Set the directory path of the input fasta file', required=True)
-    parser.add_argument('-o', '--output', help='Set the directory path of the output folder (default: cwd)', default=os.path.join(os.getcwd(), "sheared_bayes.txt"))
-    parser.add_argument('-b', '--buffer', help="the buffer size", type=int)
+    parser.add_argument('-o', '--output', help='Set the directory path of the output folder (default: cwd)', default=os.getcwd())
+    parser.add_argument('-b', '--buffer', help="the buffer size", type=int, default=1_000_000)
     parser.add_argument('-v', '--version', action='version', version='%(prog)s ')
     return parser
 
@@ -21,8 +21,8 @@ def read_fasta(fh):
     """
     :return: tuples of (title, seq)
     """
-    title = None
-    data = None
+    title = ""
+    data = ""
     for line in fh:
         if line[0] == ">":
             if title:
@@ -44,16 +44,17 @@ def grouper(iterable, n, fillvalue=None):
     return zip_longest(*args, fillvalue=fillvalue)
 
 
-def split_sequence_file_on_sample_ids(fasta, output_dir, buffer):
-    for group in grouper(read_fasta(fasta), buffer):
-        d_group = defaultdict(str)
-        for header, seq in group:
-            sample_id = header.split()[0].split("_")[0]
-            d_group[sample_id] = d_group[sample_id] + f">{header}\n{seq}\n"
-        for k, v in d_group.items():
-            outfile = os.path.join(output_dir, f"{k}.fna")
-            with open(outfile, "w+") as outfile:
-                outfile.write(v)
+def split_sequence_file(fasta, output_dir, buffer=1_000_000):
+    with open(fasta) as inf_fasta:
+        for group in grouper(read_fasta(inf_fasta), buffer):
+            d_group = defaultdict(str)
+            for header, seq in group:
+                sample_id = header.split()[0].split("_")[0]
+                d_group[sample_id] = d_group[sample_id] + f">{header}\n{seq}\n"
+            for k, v in d_group.items():
+                outfile = os.path.join(output_dir, f"{k}.fna")
+                with open(outfile, "w+") as outfile:
+                    outfile.write(v)
 
 
 def main():
@@ -65,7 +66,7 @@ def main():
     outdir = os.path.dirname(os.path.abspath(os.path.join(args.output)))
     os.makedirs(outdir, exist_ok=True)
 
-    split_sequence_file_on_sample_ids(args.fasta, args.output, args.buffer)
+    split_sequence_file(args.fasta, args.output, buffer=args.buffer)
 
     print("Execution time: %s" % (datetime.datetime.now() - start_time))
 
